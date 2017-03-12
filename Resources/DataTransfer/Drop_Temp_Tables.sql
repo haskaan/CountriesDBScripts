05be019ad58b141e06b9b3bf16c0fdb53acb99db
ByNeo <kaanhas.52@gmail.com>
2017-03-12 21:08:03 +0300

    Add Drop_Temp_Tables.sql

---
+++ Resources/DataTransfer/Drop_Temp_Tables.sql
@@ -0,0 +1,4 @@
+IF OBJECT_ID('dbo.[Ýl$]', 'U') IS NOT NULL DROP TABLE ['Ýl$']
+IF OBJECT_ID('dbo.[Ýl-Ýlçe$]', 'U') IS NOT NULL DROP TABLE ['Ýl-Ýlçe$']
+IF OBJECT_ID('dbo.[Ýl-Ýlçe-Semt$]', 'U') IS NOT NULL DROP TABLE ['Ýl-Ýlçe-Semt$']
+IF OBJECT_ID('dbo.[Ýl-Ýlçe-Semt-Mahalle-PK$]', 'U') IS NOT NULL DROP TABLE ['Ýl-Ýlçe-Semt-Mahalle-PK$']
